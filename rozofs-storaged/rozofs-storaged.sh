de080c754b0955e4ad7b49cbad2939466c5552c5
Dimitri Pertin <dimitri.pertin@univ-nantes.fr>
2015-01-14 12:32:06 +0100

    add the execution of storaged in CMD

--- rozofs-storaged/rozofs-storaged.sh
+++ rozofs-storaged/rozofs-storaged.sh
@@ -8,3 +8,5 @@ DOCKER_ROZOFS_STORAGE_ID=${DOCKER_ROZOFS_STORAGE_ID:=1}
 sed -i "s/cidToDefine/${DOCKER_ROZOFS_CLUSTER_ID}/" /etc/rozofs/storage.conf
 sed -i "s/sidToDefine/${DOCKER_ROZOFS_STORAGE_ID}/" /etc/rozofs/storage.conf
 
+echo "Start rozofs-storaged service"
+storaged
