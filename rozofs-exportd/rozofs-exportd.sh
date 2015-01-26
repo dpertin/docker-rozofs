749d3dfd51feef637b846e87c63c05c722287605
Dimitri Pertin <dimitri.pertin@univ-nantes.fr>
2015-01-26 22:12:41 +0100

    add cluster parameters management (size & layout) + getopt

--- rozofs-exportd/rozofs-exportd.sh
+++ rozofs-exportd/rozofs-exportd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DOCKER_ROZOFS_CLUSTER_SIZE=${DOCKER_ROZOFS_CLUSTER_SIZE:=4}
+DOCKER_ROZOFS_LAYOUT=${DOCKER_ROZOFS_LAYOUT:=0}
 
-function get_ip()
+function getHostIP()
 {
     local ip=$(getent hosts "$1" | cut -d " " -f1)
     echo "${ip}"
@@ -10,13 +11,30 @@ function get_ip()
 
 echo "Set the rozofs-exportd configuration file"
 
-for index in $(seq -f "%02g" "1" "${DOCKER_ROZOFS_CLUSTER_SIZE}"); do
-    name="rozofs-storaged${index}"
-    # sed -i "s/rozofs-storaged${index}/$(getip rozofs-storaged${index})/" \
-    sed -i "s/${name}/$(getent hosts "${name}" | cut -d " " -f1)/" \
+#IP_ARG=()
+#
+#for index in $(seq -f "%02g" "1" "${DOCKER_ROZOFS_CLUSTER_SIZE}"); do
+#    IP_ARG+=($(getent hosts "rozofs-storaged${index}"))
+#done
+
+# The export.conf is preset with four storage nodes
+# The following adds storage nodes in the config file if required
+for index in $(seq -f "%02g" "5" "${DOCKER_ROZOFS_CLUSTER_SIZE}"); do
+    sed -i '9i \\t\t\t\t\t{ sid = '$(expr ${index} + 0)'; host = "rozofs-storaged'${index}'"; },' \
         /etc/rozofs/export.conf
 done
 
+# Replace hostnames by IP address, required for host *storcli* to bind *storio*
+for index in $(seq -f "%02g" "1" "${DOCKER_ROZOFS_CLUSTER_SIZE}"); do
+    name="rozofs-storaged${index}"
+    hostIP="$(getHostIP ${name})"
+    sed -i "s/${name}/${hostIP}/" \
+        /etc/rozofs/export.conf
+done
+
+# Set the layout in the config file
+sed -i 's/layout = 0/layout = '${DOCKER_ROZOFS_LAYOUT}'/' \
+        /etc/rozofs/export.conf
 
 echo "Restart rpcbind"
 
--- start_cluster.sh
+++ start_cluster.sh
@@ -5,17 +5,56 @@
 #   * rozofs-exportd    (one by default)
 #   * rozofs-storaged   (four by default)
 
+function usage()
+{
+    echo "Usage: $1 options:"
+    echo "      -h|--help               - display this help"
+    echo "      -l|--layout {0,1,2}     - set the RozoFS layout"
+    echo "      -s|--size #             - set the number of storage nodes"
+    exit 1
+}
+
 set -e
 
 if env | grep -q "DOCKER_ROZOFS_DEBUG"; then
     set -x
 fi
 
+# read the options
+OPTS=$(getopt -o hl:s: -l help,layout:,size: -n $0 -- "$@")
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        -h|--help)
+            usage $0
+            break;;
+        -l|--layout)
+            DOCKER_ROZOFS_LAYOUT=$2;
+            shift 2;;
+        -s|--size)
+            DOCKER_ROZOFS_CLUSTER_SIZE=$2;
+            shift 2;;
+        --) shift ; break ;;
+    esac
+done
+
 # Fetch or set future environment variables for containers
 #   * DOCKER_ROZOFS_CLUSTER_SIZE: the number of storage nodes in the cluster
 #   * DOCKER_ROZOFS_LAYOUT:       the layout set
 DOCKER_ROZOFS_CLUSTER_SIZE=${DOCKER_ROZOFS_CLUSTER_SIZE:=4}
 DOCKER_ROZOFS_LAYOUT=${DOCKER_ROZOFS_LAYOUT:=0}
+
+min_storage=$(( ${DOCKER_ROZOFS_LAYOUT}+1 * 4))
+if (( ${DOCKER_ROZOFS_CLUSTER_SIZE} < ${min_storage} )); then
+    echo -E "At least ${min_storage} storage nodes are required for layout ${DOCKER_ROZOFS_LAYOUT}"
+    exit 1
+fi
 
 # Define a bash array to keep records of link configuration
 LINK_ARG=()
@@ -45,7 +84,7 @@ for index in $(seq -f "%02g" "1" "${DOCKER_ROZOFS_CLUSTER_SIZE}"); do
     docker run -e "DOCKER_ROZOFS_STORAGE_ID"=$(expr "${index}" + 0) \
                -P \
                --name ${name} \
-               -d denaitre/rozofs-storaged # > dev/null 2>&1
+               -d denaitre/rozofs-storaged > /dev/null 2>&1
                #-e "DOCKER_ROZOFS_CLUSTER_ID" \
 
     # This array contains the list of 'link' parameters of each storaged
@@ -61,10 +100,12 @@ echo
 
 name="rozofs-exportd"
 
-docker run -P \
+docker run -e "DOCKER_ROZOFS_LAYOUT"=${DOCKER_ROZOFS_LAYOUT} \
+           -e "DOCKER_ROZOFS_CLUSTER_SIZE"=${DOCKER_ROZOFS_CLUSTER_SIZE} \
+           -P \
            ${LINK_ARG[@]} \
            --name ${name} \
-           -d denaitre/rozofs-exportd # > dev/null 2>&1
+           -d denaitre/rozofs-exportd > /dev/null 2>&1
 echo "Successfully brought up [rozofs-exportd]"
 
 export DOCKER_ROZOFS_EXPORTD_IP=$(docker inspect -f \
